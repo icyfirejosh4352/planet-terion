95490ba0e987498529a7c8f3cc97140c8dd249f6
icyfirejosh4352 <icyfirejosh4352@gmail.com>
2026-09-15 20:21:17 +0530

    added another enemy

--- planet-terion/Scripts/icyfire/enemy2.gd
+++ planet-terion/Scripts/icyfire/enemy2.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var MoveSpeed:float = 200.0
+@export var MoveSpeed:float = 50.0
+@export var JumpSpeed:float= -500.0
 @onready var player: CharacterBody2D = $"../Player"
 @onready var detection_range: Area2D = $"Detection Range"
 @onready var down_right_check: RayCast2D = $CollisionChecks/DownRightCheck
@@ -12,15 +13,26 @@ var IsChasing:bool = false
 var MovingDir: float = 1
 var TimeSinceDmg:float = 0
 var DmgTime:float = 1
+var TimeSinceJump:float = 0
+var JumpSpacing:float = 2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	TimeSinceDmg += delta
+	TimeSinceJump += delta
 	velocity.y += (get_gravity().y * delta)
 	
 	
 	if IsChasing:
 		var direction = (player.global_position - global_position).normalized()
-		velocity.x = MoveSpeed * direction.x
+		if is_on_floor():
+			velocity.x = MoveSpeed * direction.x
+		else:
+			velocity.x = MoveSpeed * direction.x * 2
+		if player.global_position.y  + 1 < global_position.y && TimeSinceJump>JumpSpacing:
+			print("Jump")
+			TimeSinceJump = 0
+			velocity.y = JumpSpeed #- (get_gravity().y*delta)
+			
 	elif !IsChasing && is_on_floor():
 		if !left_check.is_colliding() && !right_check.is_colliding() && down_left_check.is_colliding() && down_right_check.is_colliding():
 			pass
@@ -31,7 +43,7 @@ func _process(delta: float) -> void:
 			elif right_check.is_colliding() || !down_right_check.is_colliding():
 				MovingDir = -1
 				
-		velocity.x = MoveSpeed/4 * MovingDir
+		velocity.x = MoveSpeed/2 * MovingDir
 
 	for obj in detection_range.get_overlapping_bodies():
 		if obj == player:
