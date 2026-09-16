2723416ef27cf385c2f3367544543981a94c440e
icyfirejosh4352 <icyfirejosh4352@gmail.com>
2026-09-16 20:38:22 +0530

    added blocker enemy (unfinished)

---
+++ planet-terion/Scripts/icyfire/blocker.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var MoveSpeed:float = 130.0
+@onready var player: CharacterBody2D = $"../Player"
+@onready var detection_range: Area2D = $"Detection Range"
+
+var IsBlocking:bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	IsBlocking = false
+	for obj in detection_range.get_overlapping_bodies():
+		if obj.is_in_group("Player"):
+			IsBlocking = true
+	if IsBlocking:
+		print("IsBlocking")
+
+	move_and_slide()
